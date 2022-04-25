33c544cf64b017509b97c4c96243d3b414acf541
ngocphuphamm <ngocphupham682001@gmail.com>
2022-04-26 00:14:55 +0700

    42-fix-entities

---
+++ APIRESTAURANT/server/entities/Comment.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm";
+import { Restaurant } from "./Restaurant";
+import { Customer } from "./Customer";
+
+@Index(
+  "PK__Comment__699207DF9694DB46",
+  ["idRestaurant", "idCustomer", "idComment"],
+  { unique: true }
+)
+@Entity("Comment", { schema: "dbo" })
+export class Comment {
+  @Column("varchar", { primary: true, name: "idRestaurant", length: 255 })
+  idRestaurant: string;
+
+  @Column("nvarchar", { primary: true, name: "idCustomer", length: 255 })
+  idCustomer: string;
+
+  @Column("nvarchar", { primary: true, name: "idComment", length: 255 })
+  idComment: string;
+
+  @Column("ntext", { name: "comment" })
+  comment: string;
+
+  @Column("datetime", { name: "createdAt" })
+  createdAt: Date;
+
+  @ManyToOne(() => Restaurant, (restaurant) => restaurant.comments)
+  @JoinColumn([{ name: "idRestaurant", referencedColumnName: "idRestaurant" }])
+  idRestaurant2: Restaurant;
+
+  @ManyToOne(() => Customer, (customer) => customer.comments)
+  @JoinColumn([{ name: "idCustomer", referencedColumnName: "idCustomer" }])
+  idCustomer2: Customer;
+}

cdc71a558e55492210d6343b6ee9e58b0b30205a
ngocphuphamm <ngocphupham682001@gmail.com>
2022-06-10 03:29:18 +0700

    done thong ke

--- APIRESTAURANT/server/apps/restaurant/src/admin/admin.service.ts
+++ APIRESTAURANT/server/apps/restaurant/src/admin/admin.service.ts
@@ -14,7 +14,7 @@ export class AdminService {
     async getDashBoard(idRestaurant): Promise<any> {
         try {
             const entityMnager = getManager();
-            const someQuery = await entityMnager.query(`
+            const barchart = await entityMnager.query(`
             SELECT    CONVERT(VARCHAR,TS.createdAt,3) AS Date,SUM(TS.totalMoney) AS totalMoney
             from dbo.[Transaction] TS JOIN DetailTransaction DT 
             ON TS.idTransaction = DT.idTransaction
@@ -22,10 +22,41 @@ export class AdminService {
             GROUP BY  CONVERT(VARCHAR,TS.createdAt,3)
             ORDER BY  CONVERT(VARCHAR,TS.createdAt,3)
                        `)
-            return {
-                success : true,
-                data  : someQuery
+            const countFood = await entityMnager.query(` SELECT COUNT(idFood) as countFood, R.nameRestaurant
+            FROM DetailMenu DT JOIN Restaurant R ON R.idRestaurant = DT.idRestaurant
+            WHERE DT.idRestaurant = '${idRestaurant}'
+            GROUP BY R.nameRestaurant`);
+            const countBill = await entityMnager.query(`  SELECT COUNT(DISTINCT TS.idTransaction) AS countBill
+            FROM DetailTransaction DT JOIN [Transaction] TS
+            ON TS.idTransaction = DT.idTransaction
+            WHERE DT.idRestaurant = '${idRestaurant}'  AND MONTH(TS.createdAt)  = MONTH(GETDATE()) `);
+            
+            const sumMoney = await entityMnager.query(` SELECT SUM(DISTINCT totalMoney) AS sumMoney
+            FROM DetailTransaction DT JOIN [Transaction] TS
+            ON TS.idTransaction = DT.idTransaction
+            WHERE DT.idRestaurant = '${idRestaurant}'   AND MONTH(TS.createdAt)  = MONTH(GETDATE())`)
+            if(countBill[0].countBill > 0 )
+            {
+                return {
+                    success : true,
+                    barchart,
+                    sumFood: countFood[0].countFood,
+                    nameRestaurant : countFood[0].nameRestaurant,
+                    sumBill : countBill[0].countBill,
+                    sumMoney : sumMoney[0].sumMoney,
+    
+                }
             }
+            else
+            {
+                return {
+                    success : false,
+                    msg : "NOT DATA STATISTICAL",
+                    sumFood: countFood[0].countFood,
+    
+                }
+            }
+            
         }
         catch (err) {
             return {
